0041c7b4604abb222803d4ca82a79a2bcb0b9e3b
rektide <rektide@voodoowarez.com>
2013-07-02 06:03:11 -0400

    Search and nav.

--- server.js
+++ server.js
@@ -6,9 +6,29 @@ function rand(n){
 	return Math.floor(Math.random()*n)+1
 }
 
+/**
+ * @param n number of dice
+ * @param m sides per dice
+ * @param f factor to multiply by, 100 by default
+ * @param c constant factor to add (pre factor)
+ */
+function dice(n,m,f,c){
+	f= f||100
+	c= c||0
+	for(var i= 0; i< n; ++i)
+		c+= rand(m)
+	return c*f
+}
+
+function diceDelay(n,m,f,c){
+	var ms= dice(n,m,f,c)
+	return function(a){
+		return Q.delay(a,ms)
+	}
+}
+
 function delay(a){
-	var s= rand(20)+rand(20)*1000
-	return Q.delay(s).then(function(){return a})
+	return Q.delay(a,dice(2,20,1000))
 }
 
 // get a webdriver
@@ -25,7 +45,20 @@ var wd= WD.promiseRemote().init({
 
 // don't stop can't stop won't stop
 
-
 function iter(){
-	return wd.get("http://google.com")
+	return Q.when(null).then(_search).then(_nav).then(delay).then(iter)
+}
+
+function _search(){
+	return wd.get("http://google.com").then(function(){
+		return wd.sendKeys("birds\uE007")
+	}).delay(diceDelay(2,12,100,5))
+}
+
+function _nav(){
+	return wd.findElements({strategy:"cssSelector", value:"h3 a"}).then(function(links){
+		var i= rand(links.length),
+     	  link= links[i]
+     	return wd.clickElement(link)
+	})
 }
